c3e48c3670532b159e041c8c77aef62cbdefa3b7
AntiHesus <70443185+AntiHesus@users.noreply.github.com>
2020-10-30 19:25:35 +0200

    Update serveri.lua
    fixattu

--- serveri.lua
+++ serveri.lua
@@ -6,23 +6,29 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('robomeksu:rahatarkistus')
 
-AddEventHandler('robomeksu:rahatarkistus', function(price)
+AddEventHandler('robomeksu:rahatarkistus', function()
 
 	local _source = source
 
 	local xPlayer = ESX.GetPlayerFromId(_source)
 
+	local rahaa = xPlayer.getMoney()
+
 	local price = 500
 
-	if xPlayer.getMoney() >= price then
 
-	TriggerClientEvent('robomeksu:onnistunut', source)
+	if rahaa >= price then
 
 	xPlayer.removeMoney(price)
+
+	TriggerClientEvent('robomeksu:onnistunut', source, price)
+
 	
 else
 
-	TriggerClientEvent('robomeksu:eirahaa', source)
+	moneyleft = price - rahaa
+
+	TriggerClientEvent('robomeksu:eirahaa', source, moneyleft)
 
 	end
 
